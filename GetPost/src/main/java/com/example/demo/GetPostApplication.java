e627725d71718695016619b892d7b33ebdfc92b4
JISHNUCAIRO <113664356+JISHNUCAIRO@users.noreply.github.com>
2023-02-23 22:38:35 +0530

    Add files via upload

---
+++ GetPost/src/main/java/com/example/demo/GetPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetPostApplication.class, args);
+	}
+
+}
